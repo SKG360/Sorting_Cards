826be9e7ddeab391f062828e93885c45f2c415f5
SKG360 <samuel.giles@colorado.edu>
2018-08-17 21:05:10 -0600

    Adds deck files

---
+++ lib/deck.rb
@@ -0,0 +1,13 @@
+require 'pry'
+class Deck
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+
+  end
+
+  def cards
+    @cards
+  end
+end
---
+++ test/deck_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/cards'
+require './lib/guess'
+require './lib/deck'
+require 'pry'
+
+class DeckTest < Minitest::Test
+  def test_if_three_card_objects_are_passed
+  card_1 = Card.new("3","Hearts")
+  card_2 = Card.new("4", "Clubs")
+  card_3 = Card.new("5", "Diamonds")
+
+  deck = Deck.new([card_1, card_2, card_3])
+
+  expected = [card_1, card_2, card_3]
+  assert_equal expected, deck.cards
+  end 
+end
