0894a6f9f6a1667f645515264aa66cc6668953db
SKG360 <samuel.giles@colorado.edu>
2018-08-20 05:47:42 -0600

    Adds card value hashes

--- lib/cards.rb
+++ lib/cards.rb
@@ -6,4 +6,26 @@ class Card
     @suit = suit
   end
 
+  def total_card_value
+    card_value = { "2" => 2,
+                "3" => 3,
+                "4" => 4,
+                "5" => 5,
+                "6" => 6,
+                "7" => 7,
+                "8" => 8,
+                "9" => 9,
+                "10" => 10,
+                "Jack" => 11,
+                "Queen" => 12,
+                "King" => 13,
+                "Ace" => 14,
+                    }
+     suit_value = {"Clubs" => 100,
+                    "Diamonds" => 200,
+                    "Hearts" => 300,
+                    "Spades" => 400}
+                    
+  end
+
 end
